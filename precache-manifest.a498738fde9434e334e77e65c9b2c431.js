cd9b10c67a3168bece1dac1c55019db3471e025e
zlira <olesia.hr@gmail.com>
2021-05-28 20:15:22 +0300

    Updates

---
+++ precache-manifest.a498738fde9434e334e77e65c9b2c431.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ceef6a05f08082c51b4e05b8ad539f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d43654fee8ba9dc55b07",
+    "url": "./static/css/main.f00dab68.chunk.css"
+  },
+  {
+    "revision": "70d84d4895caa5fd36d0",
+    "url": "./static/js/2.e2597756.chunk.js"
+  },
+  {
+    "revision": "d43654fee8ba9dc55b07",
+    "url": "./static/js/main.59eadf1a.chunk.js"
+  },
+  {
+    "revision": "7d6202e2464fe730b277",
+    "url": "./static/js/runtime-main.a9697dd5.js"
+  },
+  {
+    "revision": "0b4c1b2188c79ac283cb50130717903d",
+    "url": "./static/media/a.0b4c1b21.svg"
+  },
+  {
+    "revision": "5d3846e0e41b21352c70af12be904ad2",
+    "url": "./static/media/kruzhok.5d3846e0.svg"
+  },
+  {
+    "revision": "6d367ec6964e87ed7c2f5c795b9d99d5",
+    "url": "./static/media/m.6d367ec6.svg"
+  },
+  {
+    "revision": "f7ee666b486d759717ac537645c011b0",
+    "url": "./static/media/menu-24px.f7ee666b.svg"
+  },
+  {
+    "revision": "d43e36f79f1dcac726b5699ed923b8d3",
+    "url": "./static/media/o.d43e36f7.svg"
+  },
+  {
+    "revision": "c6de8992e6bd73b2cb3fa9f157512119",
+    "url": "./static/media/palychka.c6de8992.svg"
+  },
+  {
+    "revision": "7ed1d8ac6396681b8e0be86f33de3441",
+    "url": "./static/media/s.7ed1d8ac.svg"
+  },
+  {
+    "revision": "9e551de98f0853738c08e2fb9118d309",
+    "url": "./static/media/serce.9e551de9.svg"
+  },
+  {
+    "revision": "85b14cd998862031db5b45447dc4404b",
+    "url": "./static/media/soma_banerok.85b14cd9.svg"
+  },
+  {
+    "revision": "59240fd5d4955a1a65851b80f5702b77",
+    "url": "./static/media/volossia.59240fd5.png"
+  }
+]);
